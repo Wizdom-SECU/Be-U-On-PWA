245b3df0db70c059624b96726e3f04ed1ea506fb
tinnakrit <tinnkrit.n@gmail.com>
2021-04-27 22:15:46 +0700

    example view

--- src/views/Classroom.vue
+++ src/views/Classroom.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    <div>
-      <div>Category:</div>
-      <div class="nav-scroller py-1 mb-2">
-        <nav class="nav d-flex justify-content-between">
-          <a class="p-2 link-secondary" href="#">All</a>
-          <a class="p-2 link-secondary" href="#">Physics</a>
-          <a class="p-2 link-secondary" href="#">Chemistry</a>
-          <a class="p-2 link-secondary" href="#">Biology</a>
-          <a class="p-2 link-secondary" href="#">Physical Education</a>
-          <a class="p-2 link-secondary" href="#">Arts</a>
-        </nav>
+  <div class="container px-3 py-4">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span class="text-primary">Course List</span>
+    </h4>
+    <div>Category:</div>
+    <div class="nav-scroller py-1 mb-2">
+      <nav class="nav d-flex justify-content-between">
+        <a class="p-2 link-secondary" href="#">All</a>
+        <a class="p-2 link-secondary" href="#">Physics</a>
+        <a class="p-2 link-secondary" href="#">Chemistry</a>
+        <a class="p-2 link-secondary" href="#">Biology</a>
+        <a class="p-2 link-secondary" href="#">Physical Education</a>
+        <a class="p-2 link-secondary" href="#">Arts</a>
+      </nav>
+    </div>
+    <div class="row gx-3 gy-3">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course A</h5>
+            <p class="card-text">Tutor Prakit</p>
+            <a class="btn btn-success" data-bs-toggle="offcanvas" href="#offcanvasBottom" aria-controls="offcanvasBottom">Enroll</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course B</h5>
+            <p class="card-text">Tutor Annop</p>
+            <a class="btn btn-success" data-bs-toggle="offcanvas">Enroll</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel" data-bs-backdrop="true" data-bs-scroll="true">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasLabel">Offcanvas</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
       </div>
     </div>
   </div>
@@ -18,6 +48,5 @@
 
 <script>
 export default {
-
 }
 </script>
--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>Tracking</div>
+  <div class="container px-3 py-4">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span class="text-primary">Now showing</span>
+    </h4>
+    <div class="row gx-3 gy-3">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course A</h5>
+            <p class="card-text">Tutor Prakit</p>
+            <a href="#" class="btn btn-danger">Leave</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr/>
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span class="text-primary">Up coming</span>
+      <span class="badge bg-primary rounded-pill">3</span>
+    </h4>
+    <div class="row gx-3 gy-3">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course A</h5>
+            <p class="card-text">Tutor Prakit</p>
+            <a href="#" class="btn btn-primary">Enter</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course B</h5>
+            <p class="card-text">Tutor Annop</p>
+            <a href="#" class="btn btn-primary">Enter</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Course C</h5>
+            <p class="card-text">Tutor Somsri</p>
+            <a href="#" class="btn btn-primary">Enter</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
