f608816dbeeeee8ff98bd54fb55ff9fe69e8bafa
DarkFairySM <supadeerdark@gmail.com>
2021-05-02 00:30:30 +0700

    UI Courses Lists T

--- src/views/Classroom.vue
+++ src/views/Classroom.vue
@@ -17,15 +17,23 @@
     <div class="row gx-3 gy-3">
       <div class="col-sm">
         <div class="card">
+          <img src="../assets/3808949.jpg" class="card-img-top">
           <div class="card-body">
             <h5 class="card-title">Course A</h5>
             <p class="card-text">Tutor Prakit</p>
-            <a class="btn btn-success" data-bs-toggle="offcanvas" href="#offcanvasBottom" aria-controls="offcanvasBottom">Enroll</a>
+            <a
+              class="btn btn-success"
+              data-bs-toggle="offcanvas"
+              href="#offcanvasBottom"
+              aria-controls="offcanvasBottom"
+              >Enroll</a
+            >
           </div>
         </div>
       </div>
       <div class="col-sm">
         <div class="card">
+          <img src="../assets/3808949.jpg" class="card-img-top">
           <div class="card-body">
             <h5 class="card-title">Course B</h5>
             <p class="card-text">Tutor Annop</p>
@@ -34,19 +42,41 @@
         </div>
       </div>
     </div>
-    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel" data-bs-backdrop="true" data-bs-scroll="true">
+    <div
+      class="offcanvas offcanvas-bottom show"
+      tabindex="-1"
+      id="offcanvas"
+      aria-labelledby="offcanvasLabel"
+      data-bs-backdrop="false"
+      data-bs-scroll="true"
+    >
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasLabel">Offcanvas</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button
+          type="button"
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
       </div>
       <div class="offcanvas-body">
-        Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
+        Content for the offcanvas goes here. You can place just about any
+        Bootstrap component or custom elements here.
       </div>
     </div>
+    <i class="bi bi-plus-circle-fill"></i>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
+<style>
+.bi {
+  color: rgba(255, 99, 120, 1);
+  size: 500px;
+  margin-left: 280px;
+  margin-right: 30px;
+  font-size: 50px;
+}
+</style>
