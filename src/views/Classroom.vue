d1085c8caf7269849a373ff807a91a603c95befd
Supachai Supmak <supadeerdark@gmail.com>
2021-05-23 20:00:34 +0700

    no message

--- src/views/Classroom.vue
+++ src/views/Classroom.vue
@@ -6,20 +6,30 @@
     <div>Category:</div>
     <div class="nav-scroller py-1 mb-2">
       <nav class="nav d-flex justify-content-between">
-        <a class="p-2 link-secondary" href="#">All</a>
-        <a class="p-2 link-secondary" href="#">Physics</a>
-        <a class="p-2 link-secondary" href="#">Chemistry</a>
-        <a class="p-2 link-secondary" href="#">Biology</a>
-        <a class="p-2 link-secondary" href="#">Physical Education</a>
-        <a class="p-2 link-secondary" href="#">Arts</a>
+        <span class="p-1 link-secondary rounded-pill bg-primary text-light"
+          >#All</span
+        >
+        <span class="p-1 link-secondary rounded-pill bg-primary text-light"
+          >#Physics</span
+        >
+        <span class="p-1 link-secondary rounded-pill bg-primary text-light"
+          >#Chemistry</span
+        >
+        <span class="p-1 link-secondary rounded-pill bg-primary text-light"
+          >#Biology</span
+        >
+        <span class="p-1 link-secondary rounded-pill bg-primary text-light"
+          >#Physical Education</span
+        >
+        <!-- <a class="p-2 link-secondary" href="#">Arts</a> -->
       </nav>
     </div>
     <div class="row gx-3 gy-3">
       <div class="col-sm">
         <div class="card">
-          <img src="../assets/3808949.jpg" class="card-img-top">
+          <img src="../assets/3808949.jpg" class="card-img-top" />
           <div class="card-body">
-            <h5 class="card-title">Course A</h5>
+            <h5 class="card-title">Chemistry</h5>
             <p class="card-text">Tutor Prakit</p>
             <a
               class="btn btn-success"
@@ -33,21 +43,48 @@
       </div>
       <div class="col-sm">
         <div class="card">
-          <img src="../assets/3808949.jpg" class="card-img-top">
+          <img src="../assets/3808949.jpg" class="card-img-top" />
           <div class="card-body">
-            <h5 class="card-title">Course B</h5>
+            <h5 class="card-title">Science</h5>
             <p class="card-text">Tutor Annop</p>
             <a class="btn btn-success" data-bs-toggle="offcanvas">Enroll</a>
           </div>
         </div>
       </div>
     </div>
+    
     <div
-      class="offcanvas offcanvas-bottom show"
+      class="bi bi-plus-circle-fill"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasBottom"
+      aria-controls="offcanvasBottom"
+    ></div>
+    <div
+      class="offcanvas offcanvas-bottom"
       tabindex="-1"
-      id="offcanvas"
-      aria-labelledby="offcanvasLabel"
-      data-bs-backdrop="false"
+      id="offcanvasBottom"
+      aria-labelledby="offcanvasBottomLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasBottomLabel">
+          Offcanvas bottom
+        </h5>
+        <button
+          type="button"
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body small">...</div>
+    </div>
+    <!-- <div
+      class="offcanvas offcanvas-bottom"
+      tabindex="-1"
+      id="offcanvasBottom"
+      aria-labelledby="offcanvasBottomLabel"
+      data-bs-backdrop="true"
       data-bs-scroll="true"
     >
       <div class="offcanvas-header">
@@ -63,8 +100,7 @@
         Content for the offcanvas goes here. You can place just about any
         Bootstrap component or custom elements here.
       </div>
-    </div>
-    <i class="bi bi-plus-circle-fill"></i>
+    </div> -->
   </div>
 </template>
 
@@ -75,8 +111,12 @@ export default {};
 .bi {
   color: rgba(255, 99, 120, 1);
   size: 500px;
-  margin-left: 280px;
+  margin-left: 240px;
   margin-right: 30px;
   font-size: 50px;
+}
+
+.p-1 {
+  margin: 1px;
 }
 </style>
